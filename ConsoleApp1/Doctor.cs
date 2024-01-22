137526b0cb084607be5bdbd4191243aa1b88a4d6
germainsafari <GG@ee10725997111>
2024-01-22 07:19:26 +0100

    final modif

--- ConsoleApp1/Doctor.cs
+++ ConsoleApp1/Doctor.cs
@@ -4,12 +4,23 @@ class Doctor : Employee
     public string specialty;
     public string pwzNumber;
     public List<OnCallDuty> onCallDuty = new List<OnCallDuty>();
- 
+
+    // Corrected constructor for Doctor class
+    public Doctor(string name, string surname, string username, string password, string specialty, string pwzNumber)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.specialty = specialty;
+        this.pwzNumber = pwzNumber;
+    }
+
     public override void DisplayInfo()
     {
         Console.WriteLine($"{name} {surname} ({specialty})");
     }
- 
+
     public void DisplaySchedule(int month)
     {
         foreach (OnCallDuty duty in onCallDuty)
--- ConsoleApp1/Nurse.cs
+++ ConsoleApp1/Nurse.cs
@@ -1,14 +1,22 @@
 [Serializable]
-
 class Nurse : Employee
 {
     public List<OnCallDuty> onCallDuty = new List<OnCallDuty>();
- 
+
+    // Corrected constructor for Nurse class
+    public Nurse(string name, string surname, string username, string password)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+    }
+
     public override void DisplayInfo()
     {
         Console.WriteLine($"{name} {surname} (Nurse)");
     }
- 
+
     public void DisplaySchedule(int month)
     {
         foreach (OnCallDuty duty in onCallDuty)
@@ -20,4 +28,3 @@ class Nurse : Employee
         }
     }
 }
- 
